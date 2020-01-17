156a8e264b136c4772ef598a5154a69a7f57e810
Jakub Bogunia <bogunia.jakub@gmail.com>
2020-01-17 00:50:18 -0800

    Initial Commit

---
+++ thirdLabs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  thirdLabs
+//
+//  Created by Jakub Bogunia on 17/01/2020.
+//  Copyright © 2020 Jakub Bogunia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
